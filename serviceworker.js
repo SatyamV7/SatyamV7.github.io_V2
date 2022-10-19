251de624d58e2cae8b18c31263b99a2033857a68
SatyamV7 <satyamverma46@outlook.com>
2022-10-19 18:26:15 +0530

    update manifest.webmanifest

--- serviceworker.js
+++ serviceworker.js
@@ -4,8 +4,8 @@
 // Names of the two caches used in this version of the service worker.
 // Change to v2, etc. when you update any of the local resources, which will
 // in turn trigger the install event again.
-const PRECACHE = 'Static_Cache v2.2';
-const RUNTIME = 'Dynamic_Cache v2.2';
+const PRECACHE = 'Static_Cache v2.3';
+const RUNTIME = 'Dynamic_Cache v2.3';
 
 // A list of local resources we always want to be cached.
 const PRECACHE_URLS = [
