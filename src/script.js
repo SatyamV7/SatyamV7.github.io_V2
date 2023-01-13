50758c90fe22b173e27c1b5dff3e2dd9ef2c8bf9
Satyam Verma <satyamverma46@outlook.com>
2023-01-13 18:01:53 +0530

    update

--- src/script.js
+++ src/script.js
@@ -1,1 +1,25 @@
 document.addEventListener('contextmenu', event => event.preventDefault());
+
+var _myAge = document.getElementById('age')
+var _myExp = document.getElementById('exp')
+
+// To calculate age:
+var year_born = 2009;
+var d = new Date();
+var n = d.getFullYear();
+function getAge(birthYear) {
+    var currentDate = new Date();
+    var currentYear = currentDate.getFullYear();
+    age = currentYear - birthYear;
+    return age;
+}
+let calculatedAge = getAge(year_born);
+
+// To calculate Experience:
+var year_exp = 11
+let calculatedExp = calculatedAge - year_exp
+
+_myExp.innerText = calculatedExp;
+
+_myAge.innerText = calculatedAge;
+
