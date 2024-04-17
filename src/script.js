c2105643c12189b372b84aab5b0cb48709d14616
Satyam Verma <satyamverma46@outlook.com>
2024-04-17 17:39:26 +0530

    Added New Cursor

--- src/script.js
+++ src/script.js
@@ -73,6 +73,47 @@ function getInfo() {
 
 getInfo();
 
+const $bigBall = document.querySelector('.cursor__ball--big');
+const $smallBall = document.querySelector('.cursor__ball--small');
+const $hoverables = document.querySelectorAll('.hoverable');
+
+// Listeners
+document.body.addEventListener('mousemove', onMouseMove);
+for (let i = 0; i < $hoverables.length; i++) {
+    $hoverables[i].addEventListener('mouseenter', onMouseHover);
+    $hoverables[i].addEventListener('mouseleave', onMouseHoverOut);
+}
+
+// Move the cursor
+function onMouseMove(e) {
+    TweenMax.to($bigBall, .4, {
+        x: e.pageX - 15,
+        y: e.pageY - 15
+    })
+    TweenMax.to($smallBall, .1, {
+        x: e.pageX - 5,
+        y: e.pageY - 12
+    })
+}
+
+// Hover an element
+function onMouseHover() {
+    TweenMax.to($bigBall, .3, {
+        scale: 4
+    })
+}
+function onMouseOut() {
+    TweenMax.to($bigBall, .3, {
+        scale: 1
+    })
+}
+
+let links = document.querySelectorAll('a');
+for(let i = 0; i < links.length; i++) {
+    links[i].addEventListener('mouseover', onMouseHover);
+    links[i].addEventListener('mouseout', onMouseOut);
+}
+
 /*
 !function () {
     function detectDevTools(allow) {
